c3f81cca3bd326f12c1565fbe22ba200d225d909
dkersten <dkersten1994@gmail.com>
2020-08-16 21:10:46 -0400

    set up react router and vehicle container component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import './App.css';
 import { useState } from 'react';
+import VehicleContainer from './containers/VehicleContainer.js';
 
 const App = () => {
 
@@ -61,73 +62,80 @@ const App = () => {
   }
 
   return (
-    <div className="App">
-      <form style={{"marginTop": "3rem"}}>
-        <div>
-          <label style={{"paddingRight": "10px"}} htmlFor="setCarName">Name</label>
-          <input type="text" name="setCarName" value={carName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <label style={{"paddingRight": "10px"}} htmlFor="setCarManufacturer">Manufacturer</label>
-          <input type="text" name="setCarManufacturer" value={carManufacturer} onChange={handleManufacturerChange} />
-        </div>
+    <div>
+      {/* <div className="App">
+        <form style={{"marginTop": "3rem"}}>
+          <div>
+            <label style={{"paddingRight": "10px"}} htmlFor="setCarName">Name</label>
+            <input type="text" name="setCarName" value={carName} onChange={handleNameChange} />
+          </div>
+          <div>
+            <label style={{"paddingRight": "10px"}} htmlFor="setCarManufacturer">Manufacturer</label>
+            <input type="text" name="setCarManufacturer" value={carManufacturer} onChange={handleManufacturerChange} />
+          </div>
 
-        <div>
-          <label style={{"paddingRight": "10px"}} htmlFor="setCarPrice">Price</label>
-          <input type="text" name="setCarPrice" value={carPrice} onChange={handlePriceChange} />
-        </div>
+          <div>
+            <label style={{"paddingRight": "10px"}} htmlFor="setCarPrice">Price</label>
+            <input type="text" name="setCarPrice" value={carPrice} onChange={handlePriceChange} />
+          </div>
 
-        <div>
-          <label style={{"paddingRight": "10px"}} htmlFor="setCarClass">Class</label>
-          <select name="setCarClass" id="carClass" onChange={handleClassChange}>
-          <option value="">Choose Class</option>
-            <option value="super">Super</option>
-            <option value="boat">Boat</option>
-            <option value="commercial">Commercial</option>
-            <option value="compact">Compact</option>
-            <option value="coupe">Coupe</option>
-            <option value="cycle">Cycle</option>
-            <option value="emergency">Emergency</option>
-            <option value="helicopter">Helicopter</option>
-            <option value="industrial">Industrial</option>
-            <option value="military">Military</option>
-            <option value="motorcycle">Motorcycle</option>
-            <option value="muscle">Muscle</option>
-            <option value="off road">Off Road</option>
-            <option value="open wheel">Open Wheel</option>
-            <option value="plane">Plane</option>
-            <option value="sedan">Sedan</option>
-            <option value="service">Service</option>
-            <option value="sport">Sport</option>
-            <option value="sports classic">Sports Classic</option>
-            <option value="suv">SUV</option>
-            <option value="utility">Utility</option>
-            <option value="van">Van</option>
-          </select>
-        </div>
+          <div>
+            <label style={{"paddingRight": "10px"}} htmlFor="setCarClass">Class</label>
+            <select name="setCarClass" id="carClass" onChange={handleClassChange}>
+            <option value="">Choose Class</option>
+              <option value="super">Super</option>
+              <option value="boat">Boat</option>
+              <option value="commercial">Commercial</option>
+              <option value="compact">Compact</option>
+              <option value="coupe">Coupe</option>
+              <option value="cycle">Cycle</option>
+              <option value="emergency">Emergency</option>
+              <option value="helicopter">Helicopter</option>
+              <option value="industrial">Industrial</option>
+              <option value="military">Military</option>
+              <option value="motorcycle">Motorcycle</option>
+              <option value="muscle">Muscle</option>
+              <option value="off road">Off Road</option>
+              <option value="open wheel">Open Wheel</option>
+              <option value="plane">Plane</option>
+              <option value="sedan">Sedan</option>
+              <option value="service">Service</option>
+              <option value="sport">Sport</option>
+              <option value="sports classic">Sports Classic</option>
+              <option value="suv">SUV</option>
+              <option value="utility">Utility</option>
+              <option value="van">Van</option>
+            </select>
+          </div>
 
-        <div>
-          <label style={{"paddingRight": "10px"}} htmlFor="setCarAvailability">Availability</label>
-          <select name="setCarAvailability" id="availability" onChange={handleAvailabilityChange}>
-          <option value="">Select Option</option>
-            <option value="Legendary Motorsport">Legendary Motorsport</option>
-            <option value="Southern S.A. Super Autos">Southern S.A. Super Autos</option>
-            <option value="Warstock Cache & Carry">Warstock Cache & Carry</option>
-            <option value="Benny's Original Motor Works">Benny's Original Motor Works</option>
-            <option value="Elitás Travel">Elitás Travel</option>
-            <option value="DockTease">DockTease</option>
-            <option value="ArenaWar.tv">ArenaWar.tv</option>
-            <option value="Pedal and Metal Cycles">Pedal and Metal Cycles</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
+          <div>
+            <label style={{"paddingRight": "10px"}} htmlFor="setCarAvailability">Availability</label>
+            <select name="setCarAvailability" id="availability" onChange={handleAvailabilityChange}>
+            <option value="">Select Option</option>
+              <option value="Legendary Motorsport">Legendary Motorsport</option>
+              <option value="Southern S.A. Super Autos">Southern S.A. Super Autos</option>
+              <option value="Warstock Cache & Carry">Warstock Cache & Carry</option>
+              <option value="Benny's Original Motor Works">Benny's Original Motor Works</option>
+              <option value="Elitás Travel">Elitás Travel</option>
+              <option value="DockTease">DockTease</option>
+              <option value="ArenaWar.tv">ArenaWar.tv</option>
+              <option value="Pedal and Metal Cycles">Pedal and Metal Cycles</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
 
-        <div>
-          <input type="submit" onClick={handleSubmit}/>
-        </div>
-        
-      </form>
+          <div>
+            <input type="submit" onClick={handleSubmit}/>
+          </div>
+          
+        </form>
+        <NavLink style={{color: "white"}} to="/vehicles">Vehicles</NavLink>
+      </div> */}
+      <Switch>
+      <Route path="/vehicles" render={() => <VehicleContainer />} />
+    </Switch>
     </div>
+    
   );
 }
 
---
+++ src/containers/VehicleContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VehicleContainer = () => {
+    return(
+        <main>
+            Vehicle Container
+        </main>
+    )
+}
+
+export default VehicleContainer;
