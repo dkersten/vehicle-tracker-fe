bf325e4e91040aad8fa8bd6f39f2f377391867de
dkersten <dkersten1994@gmail.com>
2020-08-26 15:31:26 -0400

    small modifications

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import VehicleContainer from './containers/VehicleContainer.js';
 
 const App = () => {
 
-  const Summary = styled.p`
-      width: 96%;
-      max-width: 700px;
-      margin-left: auto;
-      margin-right: auto;
-      line-height: 1.3;
-      color: #1F2833;
-  `
+  // const Summary = styled.p`
+  //     width: 96%;
+  //     max-width: 700px;
+  //     margin-left: auto;
+  //     margin-right: auto;
+  //     line-height: 1.3;
+  //     color: #1F2833;
+  // `
 
   return (
     <div>
-      <Summary>
+      {/* <Summary>
         This is a work in progress, pet project built to show information about GTA Online. As of right now I have only compiled some information about the vehicles in game. Gathering the information is quite time consuming so as I (hopefully) have more time I will continue to add content and information. <NavLink to="/vehicles">Vehicles</NavLink>
-      </Summary>
+      </Summary> */}
       <Switch>
         <Route path="/vehicles" render={() => <VehicleContainer />} />
       </Switch>
